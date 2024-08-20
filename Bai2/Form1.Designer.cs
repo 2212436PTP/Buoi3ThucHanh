2d5b33d5575e5dc70c83cf9cb6501a8588d2a67a
PC838 <PC838@PC838>
2024-08-20 10:57:31 +0700

    Add project files.

---
+++ Bai2/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Bai2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.rdNam = new System.Windows.Forms.RadioButton();
+            this.rdNữ = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdNữ);
+            this.groupBox1.Controls.Add(this.rdNam);
+            this.groupBox1.Location = new System.Drawing.Point(100, 95);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chọn giới tính";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // rdNam
+            // 
+            this.rdNam.AutoSize = true;
+            this.rdNam.Location = new System.Drawing.Point(28, 35);
+            this.rdNam.Name = "rdNam";
+            this.rdNam.Size = new System.Drawing.Size(47, 17);
+            this.rdNam.TabIndex = 0;
+            this.rdNam.TabStop = true;
+            this.rdNam.Text = "Nam";
+            this.rdNam.UseVisualStyleBackColor = true;
+            // 
+            // rdNữ
+            // 
+            this.rdNữ.AutoSize = true;
+            this.rdNữ.Location = new System.Drawing.Point(28, 58);
+            this.rdNữ.Name = "rdNữ";
+            this.rdNữ.Size = new System.Drawing.Size(39, 17);
+            this.rdNữ.TabIndex = 0;
+            this.rdNữ.TabStop = true;
+            this.rdNữ.Text = "Nữ";
+            this.rdNữ.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MainForm";
+            this.Text = "Bài 2";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.RadioButton rdNữ;
+        private System.Windows.Forms.RadioButton rdNam;
+    }
+}
+
---
+++ Buoi3ThucHanh/MainForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Buoi3ThucHanh
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        // Click đôi vào nút chương trình sẽ tự động tạo hàm sự kiện này
+        // Khi ta click chuột vào nút chương trình sẽ làm gì
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            var tenDaNhap = txtTên.Text;
+            MessageBox.Show($"Xin chào bạn {tenDaNhap}. Rất vui được gặp bạn", "Thông điệp chào mừng");
+        }
+
+        private void btnSaoChepOK_Click(object sender, EventArgs e)
+        {
+            txtSaoChep.Text = txtTên.Text;
+        }
+
+        private void txtTên_TextChanged(object sender, EventArgs e)
+        {
+            txtSaoChep.Text = txtTên.Text;
+        }
+        // Bây giờ muốn xử lý sự kiện, trong ô tên gõ gì thì ở sao chép có nội dung như vậy, không cần nhấn nút sao chép
+        // Xử lý
+    }
+}
